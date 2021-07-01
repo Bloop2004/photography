9d4ea5c98dd9b4e027b8dadbabfcca80a2bc87d6
Bloop2004 <86659372+Bloop2004@users.noreply.github.com>
2021-07-01 15:53:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function changelook() {
+    document.getElementById("radius1").style.borderRadius = "200px";
+    document.getElementById("radius2").style.borderRadius = "200px";
+    document.getElementById("radius3").style.borderRadius = "200px";
+    document.getElementById("radius4").style.borderRadius = "200px";
+        document.getElementById("body").style.backgroundColor="blue";
+}
